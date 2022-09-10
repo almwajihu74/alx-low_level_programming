45ae13cfcdb3dc7b1832b424061aaad4290b97f4
abdulkabir <almuwajihu74@gmail.com>
2022-09-10 16:05:32 -0700

    puts

---
+++ 0x00-hello_world/4-imput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *   main  - Entry point
+ *   
+ *     Return: always zero
+ */
+int main(void)
+{
+	        puts("\"programming is lke building a multilingual puzzel\n");
+		return (0);
+}
